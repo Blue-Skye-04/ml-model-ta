4381d0a03dd2102ade48781f5a557fe223ff8337
Blue-Skye-04 <harishumaidi16@gmail.com>
2023-03-08 19:24:02 +0700

    update 03082023
    tambah sort persentase, sudah bisa menampilkan 3 kategori teratas

--- app.py
+++ app.py
@@ -6,17 +6,21 @@ with open('finalized_model.pkl', 'rb') as f:
     model, vectorizer = pickle.load(f)
     
 app = Flask(__name__)
+def takeSecond(elem):
+    return elem[1]
 
 @app.route('/', methods = ["GET","POST"])
-def marks():
+def index():
+    pred = ""
     if request.method == "POST":
         uraian = request.form["uraian"]
         simple_test = [uraian]
         simple_test_dtm = vectorizer.transform(simple_test)
-        prediction = model.predict(simple_test_dtm)
-        pred = prediction
-    
-    return render_template("index.html", prediction = pred)    
+        prediction = model.predict_proba(simple_test_dtm)
+
+        pred = [["Sistem Energi Listrik (SEL)", prediction[0][0]],["Sistem Mekatronika (SM)", prediction[0][1]],["Teknologi Informasi dan Komunikasi (ICT)", prediction[0][2]]]
+        pred.sort(key=takeSecond)
+    return render_template("index.html", prediction = pred, text = uraian)    
     
 
 if __name__ == "__main__":
